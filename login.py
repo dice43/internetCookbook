abaaf8300e3168648ba4eac59b2a5ec9f420a7aa
Hassan Koroma <ankoroma@email.wm.edu>
2022-07-22 00:30:41 +0000

    heroku

---
+++ login.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username or Email',
+                           validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
